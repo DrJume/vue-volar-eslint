8e94d419e4a45ce4225c97bee129ed77b6fd6aa7
johnsoncodehk <johnsoncodehk@gmail.com>
2022-12-14 11:20:41 +0800

    refactor: vue-eslint-parser -> @volar-plugins/eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,33 @@
 /* eslint-env node */
 
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
   ],
-  parser: 'vue-eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
-
+  // ignore <template>, <style> and SFC
+  ignorePatterns: ['**/*.vue', '**/*.html', '**/*.css', '**/*.scss'],
   overrides: [
     {
-      files: ['*.vue'],
-      extends: [
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:@typescript-eslint/strict',
-      ],
-      parser: 'vue-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        project: ['./tsconfig.json'],
-      },
-    },
-    {
-      files: ['*.ts', '*.mts'],
-      extends: [
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:@typescript-eslint/strict',
-      ],
-      parserOptions: {
-        project: ['./tsconfig.json'],
+      // <script> in .vue files
+      files: ['*.vue.ts', '*.vue.js'],
+      rules: {
+        // ignore invalid template errors in virtual code
+        'prefer-const': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'prettier/prettier': 'off',
+        // ...
       },
     },
   ],
